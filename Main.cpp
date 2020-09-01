ff96f1728b34a17e6ca0dcf63fa9a6b3e478370b
Mkoz <mefistofel_89@list.ru>
2020-09-01 11:45:04 +0300

    Create Main.cpp
    Implement different define sections

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "HelloWorld.h"
+
+int main(int argc, char *argv[]) {
+
+#ifdef MKOZYREV_COMMON_LAYER
+  printf("Absolutelly usless app for common layer\n");
+#elif defined(MKOZYREV_SEPARATE_LAYER)
+  printf("Absolutelly usless app for separate layer\n");
+#else
+  printf("Tru-tu-tu: Something whent wrong\n");
+#endif
+
+return 0;
+}
